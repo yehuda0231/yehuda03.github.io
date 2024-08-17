2c034ff67520090adef15fb78a623bf271cc8114
yehuda0231 <145263609+yehuda0231@users.noreply.github.com>
2024-08-17 21:16:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 const header = document.querySelector("header");
-window.addEventListener ("scroll", function() {
-    header.classList.toggle ("sticky", this.window.scrollY > 100);
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 100);
 });
+
+const menu = document.getElementById("menu-icon");
+const navlist = document.querySelector(".navlist");
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+};
+
+window.onscroll = () => {
+    menu.classList.remove("bx-x");
+    navlist.classList.remove("open");
+};
+
+
+function SayHello() {
+    var messageDiv = document.getElementById("message");
+    messageDiv.innerHTML = "Thank you! You have said Hello!";
+}
